bc710a02a455282c1143e8c8f8ddb8e606ab4031
Yoon-D-G <e_d_garrod@hotmail.com>
2021-12-07 22:28:58 +0000

    webscarping.py

--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,6 @@
 import requests
 import json
-#import pandas as pd
+import pandas as pd
 import re
 import datetime as dt
 from time import sleep
@@ -9,7 +9,7 @@ class Scraper:
     def __init__(self):
         pass 
     
-    def request(self):
+    def request(self, endpoint):
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -28,20 +28,37 @@ class Scraper:
             ('format', 'json'),
         )
 
-        response = requests.get('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes', headers=headers, params=params)
+        response = requests.get('https://vpic.nhtsa.dot.gov/api/vehicles/{}'.format(endpoint), headers=headers, params=params)
 
         response = json.loads(response.text)
 
-        return response
+        return response['Results']
 
-    def process(self):
-        pass
+    def process(self, results: list):
+        output = ''
+        for result in results:
+            output += self.format_line(result) + '\n'
+            # print(output)
+        return output
 
     def run(self):
-        response = self.request()
-        return response
+        all_makes = self.request('getallmakes')
+        # print(all_makes)
+        output = ''
+        counter = 0
+        for make in all_makes:
+            if counter == 5:
+                break
+            output += str(self.process(self.request('getmodelsformake/{}'.format(make['Make_Name']))))
+            counter += 1
+
+        return output
+
+    def format_line(self, list):
+        return 'ID:{id} Make:{make}'.format(id=list['Make_ID'], make=list['Make_Name'])
 
 scraper = Scraper()
 print(scraper.run())
+
 
 
