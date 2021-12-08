b752767bebd3aa289730b5c71fdc1d3ae5826014
Yoon-D-G <e_d_garrod@hotmail.com>
2021-12-08 16:32:00 +0000

    working with getmodelsformake/{MAKE} endpoint & pandas dataframe

--- webscraping.py
+++ webscraping.py
@@ -29,36 +29,52 @@ class Scraper:
         )
 
         response = requests.get('https://vpic.nhtsa.dot.gov/api/vehicles/{}'.format(endpoint), headers=headers, params=params)
+        #protect this request
+        #one scenario for 200 response (everything went fine)
+        #a different scenario for any other request code (eg 404, 503) - 10 second cooldown before another request then try again.
+        #try this 5 times and then give up and move on to the next request
+        #extension - keep track of how many requests have failed in a row. If there is a certain threshold (eg 5 requests in a row)
+        #then stop doing requests. persistent or non-persistent counter. 
 
         response = json.loads(response.text)
 
         return response['Results']
 
     def process(self, results: list):
-        output = ''
+        output = []
         for result in results:
-            output += self.format_line(result) + '\n'
-            # print(output)
-        return output
+            result_dict = {
+                'Make': result.get('Make_Name'),
+                'Model': result.get('Model_Name'),
+                'Make ID': result.get('Make_ID'),
+                'Model ID': result.get('Model_ID')
+            }
+            output.append(result_dict)
+        output_df = pd.DataFrame(output)
+        return output_df
 
     def run(self):
         all_makes = self.request('getallmakes')
-        # print(all_makes)
-        output = ''
+        responses = []
         counter = 0
         for make in all_makes:
-            if counter == 5:
+            if counter == 20:
                 break
-            output += str(self.process(self.request('getmodelsformake/{}'.format(make['Make_Name']))))
+            response = self.request('getmodelsformake/{}'.format(make['Make_Name']))
+            process_response = self.process(response)
+            responses.append(process_response)
             counter += 1
-
-        return output
+        return responses
 
     def format_line(self, list):
         return 'ID:{id} Make:{make}'.format(id=list['Make_ID'], make=list['Make_Name'])
 
-scraper = Scraper()
-print(scraper.run())
+if __name__ == '__main__':
+    scraper = Scraper()
+    for response in scraper.run():
+        print(response)
+
+
 
 
 
